b583ab8690b31e6da4827808da519fe00c051667
Chase Coleman <cc7768@gmail.com>
2025-09-30 12:42:28 +0000

    Comment out hyperliquidEvm definition and remove from index

--- src/chains/definitions/hyperliquidEvm.ts
+++ src/chains/definitions/hyperliquidEvm.ts
@@ -1,13 +1,16 @@
-import { defineChain } from '../../utils/chain/defineChain.js'
+// import { defineChain } from '../../utils/chain/defineChain.js'
 
-export const hyperliquidEvm = /*#__PURE__*/ defineChain({
-  id: 999,
-  name: 'Hyperliquid EVM',
-  nativeCurrency: { name: 'HYPE', symbol: 'HYPE', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc.hyperliquid.xyz/evm'],
-    },
-  },
-  testnet: false,
-})
+// TODO: Hyperliquid EVM chose a chain id that conflicts with wanchainTestnet
+//       See: https://github.com/ethereum-lists/chains/pull/6840
+//       Next steps: https://github.com/wevm/viem/pull/3390#issuecomment-2808052515
+// export const hyperliquidEvm = /*#__PURE__*/ defineChain({
+//   id: 999,
+//   name: 'Hyperliquid EVM',
+//   nativeCurrency: { name: 'HYPE', symbol: 'HYPE', decimals: 18 },
+//   rpcUrls: {
+//     default: {
+//       http: ['https://rpc.hyperliquid.xyz/evm'],
+//     },
+//   },
+//   testnet: false,
+// })
